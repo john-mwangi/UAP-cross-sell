673c75f1cb24ae2a4cc51b5d1af99b85f46899be
John Mwangi <mail.mwangi@gmail.com>
2021-05-04 21:56:09 +0300

    Added spinners.

--- App/uap_cross_sell/ui/main.R
+++ App/uap_cross_sell/ui/main.R
@@ -3,6 +3,7 @@ ownership <- readRDS("./objects/ownership.rds")
 
 library(shiny)
 library(DT)
+library(shinycssloaders)
 
 fluidPage(
   
@@ -50,7 +51,7 @@ sidebarLayout(
                              textInput(inputId = "customer_ids", 
                                        label = "Enter account numbers separated by commas",
                                        value = "1337,1418"),
-                             dataTableOutput(outputId = "customer_recomms")),
+                             withSpinner(dataTableOutput(outputId = "customer_recomms"))),
                     
                     tabPanel(title = "CUSTOMERS",
                              tags$p("Use this tab when you don't have a specific customer in mind but
@@ -81,7 +82,7 @@ sidebarLayout(
                                                           label = "Maximum product value", 
                                                           value = 200000))),
                              tags$hr(),
-                             dataTableOutput(outputId = "target_list")),
+                             withSpinner(dataTableOutput(outputId = "target_list"))),
                     
                     tabPanel(title = "PRODUCTS",
                              tags$p("Use this tab when you want to recommend additional products on
